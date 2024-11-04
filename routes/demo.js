763e8a253a77b6c11a8a3bd0778b3240f002ee6c
Tayrack <t.samman@me.com>
2024-11-04 16:59:13 -0500

    Add Sign up functionality

--- routes/demo.js
+++ routes/demo.js
@@ -16,7 +16,22 @@ router.get('/login', function (req, res) {
   res.render('login');
 });
 
-router.post('/signup', async function (req, res) {});
+router.post('/signup', async function (req, res) {
+  const userData = req.body;
+  const enteredEmail = userData.email;
+  const enteredConfirmEmail = userData['confirm-email'];
+  const enteredPassword = userData.password;
+
+  const user = {
+    email: enteredEmail,
+    password: enteredPassword
+  }
+
+  db.getDb().collection('users').insertOne(user);
+
+  res.redirect('/login');
+
+});
 
 router.post('/login', async function (req, res) {});
 
